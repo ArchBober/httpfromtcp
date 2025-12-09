a8bc572be56dd386b2762765d9b31cf00119321f
archbober <git@archbober.com>
2025-12-09 14:53:06 +0100

    read 8 bytes and print

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("Could not open file: %v", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	buff := make([]byte, 8)
+
+	for {
+		n, err := f.Read(buff)
+		if err != nil {
+			if err == io.EOF {
+				os.Exit(0)
+			}
+			log.Fatal("Could not read file: %v", err)
+			os.Exit(1)
+		}
+		fmt.Printf("read: %s\n", string(buff[:n]))
+	}
+
 }
