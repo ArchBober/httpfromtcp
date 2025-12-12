9a2fe30326d77a8b440ccc0e936710bffdd3e32d
archbober <git@archbober.com>
2025-12-12 01:19:22 +0100

    headers

---
+++ internal/headers/headers.go
@@ -0,0 +1,28 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+type Headers map[string]string
+
+const crlf = "\r\n"
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return 0, false, nil
+	}
+	if idx == 0 {
+		return 2, true, nil
+	}
+	trimData := strings.TrimSpace(string(data[:idx]))
+	field := strings.Split(trimData, " ")
+	if len(field) != 2 {
+		return 0, false, fmt.Errorf("wrong format too many values")
+	}
+	h[strings.Trim(field[0], ":")] = field[1]
+	return idx + 2, false, nil
+}
