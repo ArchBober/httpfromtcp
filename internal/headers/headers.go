ee923d01b80e1ec73f440b9a4f2344be06597067
archbober <git@archbober.com>
2026-01-08 21:37:58 +0100

    body

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -43,6 +44,12 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	return idx + 2, false, nil
 }
 
+func (h Headers) Get(key string) (string, bool) {
+	key = strings.ToLower(key)
+	v, ok := h[key]
+	return v, ok
+}
+
 func (h Headers) Set(key, value string) {
 	key = strings.ToLower(key)
 	v, ok := h[key]
@@ -55,16 +62,25 @@ func (h Headers) Set(key, value string) {
 	h[key] = value
 }
 
+var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+
+// validTokens checks if the data contains only valid tokens
+// or characters that are allowed in a token
 func validTokens(data []byte) bool {
-	for _, s := range data {
-		if !((s >= '0' && s <= '9') ||
-			(s >= 'A' && s <= 'Z') ||
-			(s >= '^' && s <= 'z') ||
-			(s >= '#' && s <= '\'') ||
-			(s >= '*' && s <= '.') ||
-			s == '!' || s == '|' || s == '~') {
+	for _, c := range data {
+		if !isTokenChar(c) {
 			return false
 		}
 	}
 	return true
+}
+
+func isTokenChar(c byte) bool {
+	if c >= 'A' && c <= 'Z' ||
+		c >= 'a' && c <= 'z' ||
+		c >= '0' && c <= '9' {
+		return true
+	}
+
+	return slices.Contains(tokenChars, c)
 }
